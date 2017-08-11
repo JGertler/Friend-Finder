cf55c3135a8a5a85d1f573e530e373ccaa7439ca
Jessica Gertler <jessicadgertler@gmail.com>
2017-08-11 17:42:03 -0600

    final version

---
+++ app/data/friends.js
@@ -0,0 +1,115 @@
+var friends = [
+{
+	name: "Ginger",
+	photo: "https://vignette2.wikia.nocookie.net/astoldbyginger/images/c/cc/Wallpaper-ginger-in-her-room-writing-in-her-journal-1024x768-as-told-by-ginger-12589384-1024-768.jpg/revision/latest?cb=20131008173156",
+	scores: [
+		"5",
+		"4",
+		"4",
+		"5",
+		"1",
+		"2",
+		"5",
+		"4",
+		"1",
+	] //end scores
+},
+
+
+{
+	name: "Miranda",
+	photo: "https://s-media-cache-ak0.pinimg.com/736x/cc/2c/99/cc2c990fd446ad66b54f49eaa27f7c03--funny-funny-funny-funny-stuff.jpg",
+	scores: [
+		"2",
+		"3",
+		"1",
+		"2",
+		"5",
+		"2",
+		"5",
+		"4",
+		"4",
+	] //end scores
+},
+
+
+{
+	name: "Penny",
+	photo: "https://vignette3.wikia.nocookie.net/theproudfamily/images/b/b7/Vlcsnap-2011-10-14-12h39m50s115.png/revision/latest?cb=20111215221939",
+	scores: [
+		"2",
+		"5",
+		"2",
+		"1",
+		"3",
+		"2",
+		"5",
+		"3",
+		"2",
+	] //end scores
+},
+
+
+{
+	name: "Titus",
+	photo: "https://i.elitestatic.com/content/uploads/2016/09/07221324/titus-burgess-unbreakable.jpg",
+	scores: [
+		"2",
+		"2",
+		"1",
+		"5",
+		"3",
+		"3",
+		"3",
+		"1",
+		"2",
+	] //end scores
+},
+
+
+{
+	name: "Michael",
+	photo: "http://cdn1.theodysseyonline.com/files/2015/10/30/6358177813696988401291296824_michael-scott-the-office-9.jpg",
+	scores: [
+		"5",
+		"5",
+		"5",
+		"5",
+		"5",
+		"5",
+		"5",
+		"5",
+		"5",
+	] //end scores
+},
+
+
+{
+	name: "Patrick",
+	photo: "https://upload.wikimedia.org/wikipedia/en/7/7e/Patrick_Star.png",
+	scores: [
+		"5",
+		"2",
+		"3",
+		"5",
+		"1",
+		"5",
+		"3",
+		"2",
+		"4",
+	] //end scores
+}
+
+];
+
+
+module.exports=friends;
+
+
+
+
+
+
+
+
+
